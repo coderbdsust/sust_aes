fa7875fce145c409bf7d7b533d2b16c02a0f1d8b
RaggedyCoder <bluepandora666@gmail.com>
2015-08-11 00:41:37 +0600

    লগইন পেজকে সাইন আপ এবং লগ ইন এর জন্যে আলাদাভাবে আইডেন্টি দেওয়া।

--- src/main/java/com/great/cms/controller/LoginController.java
+++ src/main/java/com/great/cms/controller/LoginController.java
@@ -1,14 +1,22 @@
 package com.great.cms.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/account")
 public class LoginController {
-	
+
 	@RequestMapping("/login")
-	public String loginPage(){
+	public String loginPage(Model uiModel) {
+		uiModel.addAttribute("login", true);
+		return "login";
+	}
+
+	@RequestMapping("/signup")
+	public String signUpPage(Model uiModel) {
+		uiModel.addAttribute("signup", true);
 		return "login";
 	}
 
