2c732cc2d2ad72ee1718b7371a82b4fe6d51d210
RaggedyCoder <bluepandora666@gmail.com>
2015-08-11 13:18:30 +0600

    login and sign up page entry style changed

--- src/main/java/com/great/cms/controller/LoginController.java
+++ src/main/java/com/great/cms/controller/LoginController.java
@@ -2,21 +2,16 @@ package com.great.cms.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/account")
 public class LoginController {
 
-	@RequestMapping("/login")
-	public String loginPage(Model uiModel) {
-		uiModel.addAttribute("login", true);
-		return "login";
-	}
-
-	@RequestMapping("/signup")
-	public String signUpPage(Model uiModel) {
-		uiModel.addAttribute("signup", true);
+	@RequestMapping("/user/{pageName}")
+	public String userEntry(@PathVariable String pageName,Model uiModel) {
+		uiModel.addAttribute(pageName, true);
 		return "login";
 	}
 
