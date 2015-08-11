eb0d4433f571039034f19d5c1606e5676674f908
RaggedyCoder <bluepandora666@gmail.com>
2015-08-11 13:36:49 +0600

    method added

--- src/main/java/com/great/cms/controller/LoginController.java
+++ src/main/java/com/great/cms/controller/LoginController.java
@@ -4,12 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/account")
 public class LoginController {
 
-	@RequestMapping("/user/{pageName}")
+	@RequestMapping(value="/user/{pageName}",method= RequestMethod.GET)
 	public String userEntry(@PathVariable String pageName,Model uiModel) {
 		uiModel.addAttribute(pageName, true);
 		return "login";
