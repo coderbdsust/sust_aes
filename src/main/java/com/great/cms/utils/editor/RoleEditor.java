e97a5777fa7b64a72e924932fd251eec97ecb6cb
RaggedyCoder <bluepandora666@gmail.com>
2015-08-14 17:26:33 +0600

    user verification phase added(not yet completed)

---
+++ src/main/java/com/great/cms/utils/editor/RoleEditor.java
@@ -0,0 +1,28 @@
+package com.great.cms.utils.editor;
+
+import java.beans.PropertyEditorSupport;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.great.cms.enums.Role;
+
+public class RoleEditor extends PropertyEditorSupport {
+	private static final Logger log = LoggerFactory.getLogger(RoleEditor.class);
+
+	@Override
+	public void setAsText(String text) {
+		for (Role authority : Role.values()) {
+			if (authority.name().equalsIgnoreCase(text)) {
+				setValue(authority);
+				break;
+			}
+		}
+	}
+
+	@Override
+	public String getAsText() {
+		log.info("RoleEditor value ={}", getValue());
+		return String.valueOf(getValue());
+	}
+}
--- src/test/java/com/great/cms/test/dao/UserDaoTest.java
+++ src/test/java/com/great/cms/test/dao/UserDaoTest.java
@@ -33,66 +33,63 @@ public class UserDaoTest {
 	@Autowired
 	private UserRoleDao userRoleDao;
 
-	
-//	@Test
-//	public void saveUser() {
-//		
-//		userDao.deleteAll();
-//		User user = new User();
-//		
-//		user.setUsername("coder_bd");
-//		user.setPassword("abcd123");
-//		user.setAccountNonExpired(true);
-//		user.setAccountNonLocked(true);
-//		user.setCredentialsNonExpired(true);
-//		user.setEnabled(true);
-//
-//		
-//		System.out.println("Saving user!");
-//		userDao.save(user);
-//		System.out.println("User saved!");
-//
-//
-//
-//		List<User> users  = userDao.findAll();
-//		for(User u:users){
-//			System.out.println(u);
-//		}
-//		assertEquals(1, users.size());
-//		
-//		boolean result  = userDao.usernameExists("coder_bd");
-//		assertEquals(true, result);
-//		
-//		User us = userDao.findByUsernameActive(user.getUsername());
-//		System.out.println(us);
-//		assertNotNull(us);
-//		
-//		List<User> activeUsers = userDao.findActiveUsers();
-//		assertEquals(1, activeUsers.size());
-//		for(User u:activeUsers){
-//			System.out.println(u);
-//		}
-//	}
-	
+	// @Test
+	// public void saveUser() {
+	//
+	// userDao.deleteAll();
+	// User user = new User();
+	//
+	// user.setUsername("coder_bd");
+	// user.setPassword("abcd123");
+	// user.setAccountNonExpired(true);
+	// user.setAccountNonLocked(true);
+	// user.setCredentialsNonExpired(true);
+	// user.setEnabled(true);
+	//
+	//
+	// System.out.println("Saving user!");
+	// userDao.save(user);
+	// System.out.println("User saved!");
+	//
+	//
+	//
+	// List<User> users = userDao.findAll();
+	// for(User u:users){
+	// System.out.println(u);
+	// }
+	// assertEquals(1, users.size());
+	//
+	// boolean result = userDao.usernameExists("coder_bd");
+	// assertEquals(true, result);
+	//
+	// User us = userDao.findByUsernameActive(user.getUsername());
+	// System.out.println(us);
+	// assertNotNull(us);
+	//
+	// List<User> activeUsers = userDao.findActiveUsers();
+	// assertEquals(1, activeUsers.size());
+	// for(User u:activeUsers){
+	// System.out.println(u);
+	// }
+	// }
+
 	@Test
-	public void userCheck(){
-//		List<User> users  = userDao.findAll();
-//		System.out.println(users);
-		
+	public void userCheck() {
+		// List<User> users = userDao.findAll();
+		// System.out.println(users);
+
 		User user = userDao.findByUsername("coder_bd");
 		System.out.println(user);
 		assertNotNull(user);
-		
+
 		List<User> users = userDao.findAll();
-		for(User u:users){
+		for (User u : users) {
 			System.out.println(u);
 		}
 		System.out.println(users.size());
-		
+
 		Integer count = userDao.countVerificationNeed();
-		System.out.println(count);
+		System.out.println(userDao.nonVerifiedUsers());
 	}
-	
-	
 
 }
