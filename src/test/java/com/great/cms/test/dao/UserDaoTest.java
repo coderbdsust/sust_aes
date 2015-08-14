cae54d208f6d0284bb8b51a2651c5a0f50af9132
RaggedyCoder <bluepandora666@gmail.com>
2015-08-14 17:39:03 +0600

    unit test added again

---
+++ src/test/java/com/great/cms/test/dao/UserDaoTest.java
@@ -0,0 +1,88 @@
+package com.great.cms.test.dao;
+
+import static org.junit.Assert.assertNotNull;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.great.cms.dao.UserDao;
+import com.great.cms.dao.UserRoleDao;
+import com.great.cms.entity.User;
+
+@ContextConfiguration("file:src/main/webapp/WEB-INF/sustaes-servlet.xml")
+@RunWith(SpringJUnit4ClassRunner.class)
+@Transactional
+public class UserDaoTest {
+
+	@Autowired
+	private UserDao userDao;
+
+	@Autowired
+	private UserRoleDao userRoleDao;
+
+	// @Test
+	// public void saveUser() {
+	//
+	// userDao.deleteAll();
+	// User user = new User();
+	//
+	// user.setUsername("coder_bd");
+	// user.setPassword("abcd123");
+	// user.setAccountNonExpired(true);
+	// user.setAccountNonLocked(true);
+	// user.setCredentialsNonExpired(true);
+	// user.setEnabled(true);
+	//
+	//
+	// System.out.println("Saving user!");
+	// userDao.save(user);
+	// System.out.println("User saved!");
+	//
+	//
+	//
+	// List<User> users = userDao.findAll();
+	// for(User u:users){
+	// System.out.println(u);
+	// }
+	// assertEquals(1, users.size());
+	//
+	// boolean result = userDao.usernameExists("coder_bd");
+	// assertEquals(true, result);
+	//
+	// User us = userDao.findByUsernameActive(user.getUsername());
+	// System.out.println(us);
+	// assertNotNull(us);
+	//
+	// List<User> activeUsers = userDao.findActiveUsers();
+	// assertEquals(1, activeUsers.size());
+	// for(User u:activeUsers){
+	// System.out.println(u);
+	// }
+	// }
+
+	@Test
+	public void userCheck() {
+		// List<User> users = userDao.findAll();
+		// System.out.println(users);
+
+		User user = userDao.findByUsername("coder_bd");
+		System.out.println(user);
+		assertNotNull(user);
+
+		List<User> users = userDao.findAll();
+		for (User u : users) {
+			System.out.println(u);
+		}
+		System.out.println(users.size());
+
+		Integer count = userDao.countVerificationNeed();
+		System.out.println(userDao.nonVerifiedUsers());
+	}
+
+}
