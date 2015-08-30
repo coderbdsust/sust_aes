4caea4c7d4d825156562567e41a7360013b64360
Biswajit Debnath <biswajit.sust@gmail.com>
2015-08-30 20:34:11 +0600

    Course Service Implemented and Course Controller Updated

--- src/main/java/com/great/cms/service/CourseService.java
+++ src/main/java/com/great/cms/service/CourseService.java
@@ -1,8 +1,20 @@
 package com.great.cms.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Component;
+
+import com.great.cms.entity.Course;
 
 
 public interface CourseService {
+	
+	public void saveOrUpdate(Course course);
 
+	public Course findById(Integer id);
+	
+	public List<Course> getCourses();
+	
+	public void deleteCourse(Course course);
+	
 }
