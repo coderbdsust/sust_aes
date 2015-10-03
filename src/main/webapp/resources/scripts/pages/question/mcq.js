6e4abcc2694975571164a71d8409ba8a5bc88db7
Biswajit Debnath <biswajit.sust@gmail.com>
2015-10-04 00:25:15 +0600

    Question JavaScript Working Example Added

--- src/main/webapp/resources/scripts/pages/question/mcq.js
+++ src/main/webapp/resources/scripts/pages/question/mcq.js
@@ -1,9 +1,22 @@
-//function submitMCQ() {
-//	console.log('Hellow');
-//}
-
-funtion(){
-	function submitMCQ() {
-		console.log('Hellow');
+function submitMCQ() {
+	var options = [];
+	for (var i = 1; i <= 5; i++) {
+		var opt = {};
+		opt["text"] = document.getElementById('mcq-opt-' + i + '-text').value;
+		opt["status"] = document.getElementById('mcq-opt-' + i + '-status').checked == true ? true
+				: false;
+		options.push(opt);
 	}
-}();
+	
+	var question = {};
+	question['question_text'] = document.getElementById('mcq-question-text').value;
+	question['marks'] = document.getElementById('mcq-marks').value;
+	question['time'] = document.getElementById('mcq-time').value;
+	question['question_type_id'] = 1;
+	question['question_type_name'] = "mcq";
+	question['question_body'] = options;
+
+	var data = JSON.stringify(question);
+	console.log(data);
+
+}
