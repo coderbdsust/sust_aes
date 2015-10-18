299243fda0f68fc8753f41885898d3d203e6857d
RaggedyCoder <bluepandora666@gmail.com>
2015-10-18 16:51:33 +0600

    only right side question will be added

--- src/main/webapp/resources/scripts/pages/question/add-remove.js
+++ src/main/webapp/resources/scripts/pages/question/add-remove.js
@@ -1,9 +1,30 @@
-function addQuestions(id) {
+function addQuestions(id,iterId) {
 		//alert('parent1');
 		console.log('SQ: '+id);
 		var oldParent = document.getElementById("question-bank");
 		var newParent = document.getElementById("quiz-question");
+		var hiddenInputNameQuesion="quizQuestions["+iterId+"].questionId.questionId";
+		var hiddenInputNameQuiz="quizQuestions["+iterId+"].quizId.quizId";
+		
+		var hiddenInputFieldQuestion= document.getElementById(hiddenInputNameQuesion);
+		var hiddenInputFieldQuiz= document.getElementById(hiddenInputNameQuiz);
 		//alert('parent2');
+		var questionInputField = document.createElement("input");
+		questionInputField.setAttribute('id', 'hiddenQuestionInput'+iterId);
+		questionInputField.setAttribute('type', 'hidden');
+		questionInputField.setAttribute('name', hiddenInputNameQuesion);
+		questionInputField.setAttribute('value', hiddenInputFieldQuestion.textContent);
+
+		var quizInputField = document.createElement("input");
+		quizInputField.setAttribute('id', 'hiddenQuizInput'+iterId);
+		quizInputField.setAttribute('type', 'hidden');
+		quizInputField.setAttribute('name', hiddenInputNameQuiz);
+		quizInputField.setAttribute('value', hiddenInputFieldQuiz.textContent);
+
+		var hiddenQuizQuestionHolder = document.getElementById("hiddenQuizQuestionHolder-"+iterId); 
+		hiddenQuizQuestionHolder.appendChild(questionInputField);
+		hiddenQuizQuestionHolder.appendChild(quizInputField);
+
 		var child = document.getElementById("question-" + id);
 		console.log("question-" + id);
 		$("#add-" + id).hide();
@@ -13,10 +34,16 @@ function addQuestions(id) {
 		newParent.appendChild(child);
 		//alert('parent');
 	}
-	function removeQuestions(id) {
+	function removeQuestions(id,iterId) {
 		//alert('parent1');
 		var oldParent = document.getElementById("quiz-question");
 		var newParent = document.getElementById("question-bank");
+		var hiddenInputFieldQuestion= document.getElementById('hiddenQuestionInput'+iterId);
+		var hiddenInputFieldQuiz= document.getElementById('hiddenQuizInput'+iterId);
+		var hiddenQuizQuestionHolder = document.getElementById("hiddenQuizQuestionHolder-"+iterId); 
+		hiddenQuizQuestionHolder.removeChild(hiddenInputFieldQuiz);
+		hiddenQuizQuestionHolder.removeChild(hiddenInputFieldQuestion);
+
 		//alert('parent2');
 		var child = document.getElementById("question-" + id);
 		$("#remove-" + id).hide();
--- src/main/webapp/resources/scripts/pages/question/append-question.js
+++ src/main/webapp/resources/scripts/pages/question/append-question.js
@@ -2,8 +2,18 @@ function appendQuestion(data) {
 	
 	var questionId = data['questionId'];
 	var questionText = data['questionText'];
-	//console.log(questionId+' '+questionText);
-	var item = $('<li id="question-'+questionId+'" style="background: rgba(223, 240, 216, 0.70);  margin-bottom: 2px;"><div class="task-checkbox"><p></p></div> <div class="task-title"> <span class="task-title-sp">'+questionText+' </span></div><div class="task-config"><div class="task-config-btn btn-group"><a class="btn btn-xs default" data-toggle="dropdown" data-hover="dropdown" data-close-others="true"> <i class="fa fa-cog"></i><i class="fa fa-angle-down"></i></a><ul class="dropdown-menu pull-right"><li id="add-'+questionId+'"><a id="addButton0" name="addButton'+questionId+'" type="button" onclick="addQuestions('+questionId+')"> <i class="fa fa-plus"></i> Add</a></li><li style="display: none;" id="remove-'+questionId+'"><a id="removeButton'+questionId+'" name="removeButton'+questionId+'" type="button" onclick="removeQuestions('+questionId+')"> <i class="fa fa-times"></i> Remove</a></li><li><a> <i class="fa fa-info"></i> Details</a></li></ul></div></div></li>');
+	// console.log(questionId+' '+questionText);
+	var totalQuestion= document.getElementById("hiddenTotalQuestion").textContent;
+	var quizId= document.getElementById("hiddenQuizId").textContent;
+	var inputField="<ul id=\"hiddenQuizQuestionHolder-"+totalQuestion+"\"> " +
+	"<li style=\"display: none;\"" +
+	"id=\"quizQuestions["+(totalQuestion)+"].questionId.questionId\" " +
+	">"+questionId+"</li>" +
+	"<li style=\"display: none;\" " +
+	"id=\"quizQuestions["+(totalQuestion)+"].quizId.quizId\"" +
+	">"+quizId+"</li>" +
+	"</ul>";
+	var item = $('<li id="question-'+questionId+'" style="background: rgba(223, 240, 216, 0.70);  margin-bottom: 2px;"><div class="task-checkbox"><p></p></div> <div class="task-title"> <span class="task-title-sp">'+questionText+' </span>'+inputField+'</div><div class="task-config"><div class="task-config-btn btn-group"><a class="btn btn-xs default" data-toggle="dropdown" data-hover="dropdown" data-close-others="true"> <i class="fa fa-cog"></i><i class="fa fa-angle-down"></i></a><ul class="dropdown-menu pull-right"><li id="add-'+questionId+'"><a id="addButton0" name="addButton'+questionId+'" type="button" onclick="addQuestions('+questionId+','+totalQuestion+')"> <i class="fa fa-plus"></i> Add</a></li><li style="display: none;" id="remove-'+questionId+'"><a id="removeButton'+questionId+'" name="removeButton'+questionId+'" type="button" onclick="removeQuestions('+questionId+','+totalQuestion+')"> <i class="fa fa-times"></i> Remove</a></li><li><a> <i class="fa fa-info"></i> Details</a></li></ul></div></div></li>');
 	$('#question-bank').append(item);
+	document.getElementById("totalQuestion").textContent=(++totalQuestion);
 }
-
