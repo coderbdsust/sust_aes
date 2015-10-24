6870250d306f747684af1f71167be81e7c78b195
Biswajit Debnath <biswajit.sust@gmail.com>
2015-10-24 14:11:45 +0600

    Question Type Label Added to Add Question Page!

--- src/main/webapp/resources/scripts/pages/question/append-question.js
+++ src/main/webapp/resources/scripts/pages/question/append-question.js
@@ -3,6 +3,15 @@ function appendQuestion(data) {
 	var questionId = data['questionId'];
 	var questionText = data['questionText'];
 	var requiredTime = data['requiredTime'];
+	var questionType = data['questionType'];
+	var type='';
+	if(questionType=='MCQ')
+		type='[M]';
+	else if(questionType=='DESCRIPTIVE')
+		type='[D]';
+	else
+		type='[F]';
+	
 	// console.log(questionId+' '+questionText);
 	var totalQuestion= document.getElementById("hiddenTotalQuestion").textContent;
 	var quizId= document.getElementById("hiddenQuizId").textContent;
@@ -14,7 +23,7 @@ function appendQuestion(data) {
 	"id=\"quizQuestions["+(questionId)+"].quizId.quizId\"" +
 	">"+quizId+"</li>" +
 	"</ul>";
-	var item = $('<li id="question-'+questionId+'" style="background: rgba(223, 240, 216, 0.70);  margin-bottom: 2px;"><span style="display: none;" id="required-time-'+questionId+'">'+requiredTime+'</span>	<div class="task-checkbox"><p></p></div> <div class="task-title"> <span class="task-title-sp">'+questionText+' </span>'+inputField+'</div><div class="task-config"><div class="task-config-btn btn-group"><a class="btn btn-xs default" data-toggle="dropdown" data-hover="dropdown" data-close-others="true"> <i class="fa fa-cog"></i><i class="fa fa-angle-down"></i></a><ul class="dropdown-menu pull-right"><li id="add-'+questionId+'"><a id="addButton0" name="addButton'+questionId+'" type="button" onclick="addQuestions('+questionId+')"> <i class="fa fa-plus"></i> Add</a></li><li style="display: none;" id="remove-'+questionId+'"><a id="removeButton'+questionId+'" name="removeButton'+questionId+'" type="button" onclick="removeQuestions('+questionId+')"> <i class="fa fa-times"></i> Remove</a></li><li><a> <i class="fa fa-info"></i> Details</a></li></ul></div></div></li>');
+	var item = $('<li id="question-'+questionId+'" style="background: rgba(223, 240, 216, 0.70);  margin-bottom: 2px;"><span style="display: none;" id="required-time-'+questionId+'">'+requiredTime+'</span>	<div class="task-checkbox"><p style="padding-left:5px; color:green;">'+type+'</p></div> <div class="task-title"> <span class="task-title-sp">'+questionText+' </span>'+inputField+'</div><div class="task-config"><div class="task-config-btn btn-group"><a class="btn btn-xs default" data-toggle="dropdown" data-hover="dropdown" data-close-others="true"> <i class="fa fa-cog"></i><i class="fa fa-angle-down"></i></a><ul class="dropdown-menu pull-right"><li id="add-'+questionId+'"><a id="addButton0" name="addButton'+questionId+'" type="button" onclick="addQuestions('+questionId+')"> <i class="fa fa-plus"></i> Add</a></li><li style="display: none;" id="remove-'+questionId+'"><a id="removeButton'+questionId+'" name="removeButton'+questionId+'" type="button" onclick="removeQuestions('+questionId+')"> <i class="fa fa-times"></i> Remove</a></li><li><a> <i class="fa fa-info"></i> Details</a></li></ul></div></div></li>');
 	$('#question-bank').append(item);
 	document.getElementById("totalQuestion").textContent=(++totalQuestion);
 	
