c43c783d4aa6a19c6bfd4850af61120bcba6e1e8
Biswajit Debnath <biswajit.sust@gmail.com>
2015-10-27 15:21:16 +0600

    Student Module and CourseRegistrationDao Updated

--- src/main/java/com/great/cms/dao/CourseRegistrationDao.java
+++ src/main/java/com/great/cms/dao/CourseRegistrationDao.java
@@ -4,10 +4,12 @@ import java.util.List;
 
 import com.great.cms.entity.Course;
 import com.great.cms.entity.CourseRegistration;
+import com.great.cms.entity.Student;
 
 
 public interface CourseRegistrationDao extends GenericDao<CourseRegistration, Integer>{
 
 	List<CourseRegistration> findByCourseId(Course course);
+	public List<CourseRegistration> findByIsApproved(Student student);
 
 }
