9011a34414a0cdcff0309b79940053d70b2807a9
Biswajit Debnath <biswajit.sust@gmail.com>
2015-11-10 12:39:56 +0600

    Dynamic Checkbox Style and Listener Problem Solved!

--- src/main/webapp/resources/scripts/pages/question/append-question.js
+++ src/main/webapp/resources/scripts/pages/question/append-question.js
@@ -25,7 +25,7 @@ function appendQuestion(data) {
 	"</ul>";
 	var item = $('<li id="question-'+questionId+'" style="background: rgba(223, 240, 216, 0.70);  margin-bottom: 2px;"><span style="display: none;" id="required-time-'+questionId+'">'+requiredTime+'</span>	<div class="task-checkbox"><p style="padding-left:5px; color:green;">'+type+'</p></div> <div class="task-title"> <span class="task-title-sp">'+questionText+' </span>'+inputField+'</div><div class="task-config"><div class="task-config-btn btn-group"><a id="addButton'+questionId+'" name="addButton'+questionId+'" type="button" onclick="addQuestions('+questionId+')"><i class="fa fa-plus"></i> Add</a> <a id="removeButton'+questionId+'" name="removeButton'+questionId+'" type="button" onclick="removeQuestions('+questionId+')" style="display:none;"><i class="fa fa-times"></i> Remove</a></div></div></li>');
 	$('#question-bank').append(item);
-	document.getElementById("totalQuestion").textContent=(++totalQuestion);
+	//document.getElementById("totalQuestion").textContent=(++totalQuestion);
 	
 	 
 	
--- src/main/webapp/resources/scripts/pages/question/mcq-option-maker.js
+++ src/main/webapp/resources/scripts/pages/question/mcq-option-maker.js
@@ -1,25 +1,50 @@
+function handleClick(state, id) {
+	$('#uniform-mcq-opt-' + id + '-answer').click(function() {
+		if (state) {
+			$('span:first', this).attr('class', '');
+			state = false;
+		} else {
+			$('span:first', this).attr('class', 'checked');
+			state = true;
+		}
+		var updateHandleClick = 'handleClick('+state+','+id+')';
+		$('#mcq-opt-'+id+'-answer').attr('onclick',updateHandleClick);
+	})
+}
+
 function addOption(optionNo) {
-	var newOptionNo = optionNo+1;
-	var newOptionLabel = "Option "+(newOptionNo+1);
-	var optionDiv = $('<div id="mcq-opt-'+newOptionNo+'" class="form-group"><label class="col-md-3 control-label">'+newOptionLabel+'</label><div class="row"><div class="col-md-6"><div class="input-group"><input id="mcq-opt-'+newOptionNo+'-text" type="text" class="form-control" /> <span class="input-group-addon"><div class="checker" id="uniform-mcq-opt-'+newOptionNo+'-answer"> <span><input id="mcq-opt-'+newOptionNo+'-answer" type="checkbox" /></span></div></span></div></div></div></div>');
-	$('#mcq-opt-'+optionNo).after(optionDiv);
-	var newAddFunction = 'addOption('+newOptionNo+')';
-	console.log(newAddFunction);
-	var newRemoveFunction = 'removeOption('+newOptionNo+')';
-	
+	var newOptionNo = optionNo + 1;
+	var newOptionLabel = "Option " + (newOptionNo + 1);
+	var optionDiv = $('<div id="mcq-opt-' + newOptionNo
+			+ '" class="form-group">'
+			+ '<label class="col-md-3 control-label">' + newOptionLabel
+			+ '</label>' + '<div class="row"><div class="col-md-6">'
+			+ '<div class="input-group">' + '<input id="mcq-opt-' + newOptionNo
+			+ '-text" type="text" class="form-control" />'
+			+ ' <span class="input-group-addon">'
+			+ '<div class="checker" id="uniform-mcq-opt-' + newOptionNo
+			+ '-answer"><span><input id="mcq-opt-' + newOptionNo
+			+ '-answer" type="checkbox" onclick="handleClick(false,' + newOptionNo
+			+ ')"/>' + '</span></div></span></div></div></div></div>');
+	$('#mcq-opt-' + optionNo).after(optionDiv);
+	var newAddFunction = 'addOption(' + newOptionNo + ')';
+	var newRemoveFunction = 'removeOption(' + newOptionNo + ')';
+
 	$('#addOptionBtn').attr('onclick', newAddFunction);
 	$('#removeOptionBtn').attr('onclick', newRemoveFunction);
+	// $('#uniform-mcq-opt-'+newOptionNo+'-answer').addClass('checker');
+	// console.log('CHECKER CLASS ADDED');
 }
-	
+
 function removeOption(optionNo) {
 	console.log('option:' + optionNo);
-	if(optionNo>1){
-		$('#mcq-opt-'+optionNo).remove();
-		
-		var newOptionNo=optionNo-1;
-		var newAddFunction = 'addOption('+newOptionNo+')';
-		var newRemoveFunction = 'removeOption('+newOptionNo+')';
-		
+	if (optionNo > 1) {
+		$('#mcq-opt-' + optionNo).remove();
+
+		var newOptionNo = optionNo - 1;
+		var newAddFunction = 'addOption(' + newOptionNo + ')';
+		var newRemoveFunction = 'removeOption(' + newOptionNo + ')';
+
 		$('#addOptionBtn').attr('onclick', newAddFunction);
 		$('#removeOptionBtn').attr('onclick', newRemoveFunction);
 	}
